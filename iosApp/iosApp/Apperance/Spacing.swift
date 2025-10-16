b9d1490f6e34187e373a1bacd5c400eeb803ed6e
davidsoukup <david@soukup.dev>
2025-10-16 17:53:39 +0200

    chore(iOS): added enum with app spacings

---
+++ iosApp/iosApp/Apperance/Spacing.swift
@@ -0,0 +1,22 @@
+//
+//  Spacing.swift
+//  iosApp
+//
+//  Created by David Soukup on 16.10.2025.
+//
+
+import SwiftUI
+
+enum Spacing {
+    // Elements
+    static let xSmall: CGFloat = 4
+    static let small: CGFloat = 8
+    static let medium: CGFloat = 16
+    static let large: CGFloat = 24
+    static let xLarge: CGFloat = 32
+    
+    enum Screen {
+        static let horizontal: CGFloat = 16
+        static let vertical: CGFloat = 20
+    }
+}
